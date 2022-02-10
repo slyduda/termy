4aea1fc7432cef9f74ed20c6dcbfa12c110d29fa
Sylvester Duda <sylvester.duda@gmail.com>
2022-02-09 16:13:15 -0800

    added v if and changed axios

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -312,7 +312,7 @@ const store = createStore({
                 won: game.won
             }
 
-            axios.post('http://192.168.1.4:5000/submit', payload)
+            axios.post('https://termy.gg/submit', payload)
                 .then((response) => {
                     state.submitted = true
                     
